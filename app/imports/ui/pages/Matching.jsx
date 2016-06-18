5a3eaf484a16b68761d252635564a030eac89c54
marchdown <marchdown@gmail.com>
2016-06-18 20:15:12 +0200

    joinConversation refactoring

--- app/imports/ui/pages/Matching.jsx
+++ app/imports/ui/pages/Matching.jsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { Session } from 'meteor/session';
+// import { Router } from 'react-router'
+import { browserHistory } from 'react-router'
 
 class Matching extends Component {
 
-  role() {
-    let role = (this.props.params.role == 'human') ? 'bear' : 'human';
-    return role;
+  lookingFor() {
+    let lookingFor = (this.props.params.role == 'human') ? 'bear' : 'human';
+    return lookingFor;
   }
   
   render() {
+    console.log("entered render() in Matching.jsx")
     //on render -> send a call to the server asking to put us into a relevant conversation
-    Meteor.call('joinConversation', Session.get('user-uuid'), this.role());
-
+    Meteor.call('joinConversation',
+                 Session.get('user-uuid'), 
+                 this.props.params.role,   
+                 function(error, conversationId){
+                    console.log("got a callback from joinConversation with ", conversationId)
+                    if (conversationId) {
+                      console.log("routing to '/chats/"+conversationId+"'")
+//                      console.log("this is ", this)
+                      browserHistory.push('/chats/'+conversationId)
+//                      this.history.pushState(null, 'chats/'+conversationId)
+                    }
+                  });
     //render waiting page
     return (
         <div className="page matching-page">
-        <h3>Sit tight, your {this.role()}  is on the way...</h3>
+        <h3>Sit tight, your {this.lookingFor()}  is on the way...</h3>
         </div>
     )
   }
